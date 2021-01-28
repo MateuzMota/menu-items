34b7bbbfc4c0a4361a37c62d5b362f0177a5bc2c
Mateus Mota <m4t3u5.m0t4@gmail.com>
2021-01-28 16:38:56 -0300

    chore: config and run project

--- menu-api/src/index.ts
+++ menu-api/src/index.ts
@@ -2,14 +2,41 @@
  * Required External Modules
  */
 
+import * as dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import { POINT_CONVERSION_COMPRESSED } from 'constants';
+
+const app = express();
+
+dotenv.config();
+
 /**
  * App Variables
  */
+
+ // check if the application loaded the env variable PORT. If not, keep running.
+if(!process.env.PORT) {
+    process.exit(1);
+}
+
+const PORT: number = parseInt(process.env.PORT as string, 10);
+
+console.log(PORT)
 
 /**
  * App Configuration
  */
 
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
 /**
  * Server Activation
  */
+
+ app.listen(PORT, () => {
+     console.log(`Listening on http://localhost:${PORT} :D`);
+ })
