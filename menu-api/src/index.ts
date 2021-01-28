5690d0863b56aaf3e14c47c8fa18b904f9cb7070
Mateus Mota <m4t3u5.m0t4@gmail.com>
2021-01-28 18:05:32 -0300

    chore: add routes

--- menu-api/src/index.ts
+++ menu-api/src/index.ts
@@ -6,7 +6,7 @@ import * as dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
-import { POINT_CONVERSION_COMPRESSED } from 'constants';
+import { itemsRouter } from './items/items.router';
 
 const app = express();
 
@@ -32,6 +32,7 @@ console.log(PORT)
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use('/api/menu/items', itemsRouter);
 
 /**
  * Server Activation
