26337246d96cd51fe9fff05c2f6cdb3329f96879
Mateus Mota <m4t3u5.m0t4@gmail.com>
2021-01-28 17:30:15 -0300

    chore: add service methods

---
+++ menu-api/src/items/item.interface.ts
@@ -0,0 +1,12 @@
+// Interface
+
+export interface BaseItem {
+    name: string,
+    price: number;
+    description: string;
+    image: string;
+}
+
+export interface Item extends BaseItem {
+    id: number;
+}
